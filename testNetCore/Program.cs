363d15e2bb4d227e37d7d42404f3e37c82687315
alexyap1205 <alexyap1205@live.com>
2018-07-04 18:09:32 +1000

    Pub Sub NetMQ

---
+++ testNetCore/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MessagePack;
+using NetMQ;
+using NetMQ.Sockets;
+using Newtonsoft.Json;
+using Serilog;
+using Serilog.Context;
+using Serilog.Events;
+using Serilog.Formatting.Json;
+
+namespace testNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TestSerilog();
+
+            using (var subSocket = new SubscriberSocket())
+            {
+                var tcpLocalhost = "tcp://localhost:12345";
+                subSocket.Connect(tcpLocalhost);
+                subSocket.SubscribeToAnyTopic();
+                Console.WriteLine("Waiting to Connect");
+
+                var connectMessage = subSocket.ReceiveMultipartMessage();
+                if (connectMessage[0].ConvertToString(Encoding.UTF8) == "CONNECT")
+                {
+                    Console.WriteLine("Connected");
+                    
+                    var port = connectMessage[1].ConvertToString(Encoding.UTF8);
+                    using (var reqSocket = new RequestSocket(port))
+                    {
+                        var syncMessage = new NetMQMessage();
+                        syncMessage.Append(new NetMQFrame("SYNC", Encoding.UTF8));
+                        syncMessage.Append(new NetMQFrame(tcpLocalhost, Encoding.UTF8));
+                        reqSocket.SendMultipartMessage(syncMessage);
+                        Console.WriteLine("Syncing");
+                        var syncOk = reqSocket.ReceiveFrameString();
+                        Console.WriteLine($"Synced: {syncOk}");
+                    }
+
+                    using (var poller = new NetMQPoller())
+                    {
+                        poller.Add(subSocket);
+
+                        subSocket.ReceiveReady += (sender, eventArgs) =>
+                        {
+                            var subMessage = eventArgs.Socket.ReceiveMultipartMessage();
+                            var topic = subMessage[0].ConvertToString(Encoding.UTF8);
+                            if (topic == "TopicA")
+                            {
+                                var decodedMessage = MessagePackSerializer.Deserialize<string>(subMessage[1].ToByteArray());
+                                Console.WriteLine(
+                                    $"{subMessage[0].ConvertToString(Encoding.UTF8)}: {decodedMessage}");
+                            }
+                        };
+
+                        poller.RunAsync();
+
+                        Console.WriteLine("Press Enter to Exit");
+                        Console.ReadLine();
+                        
+                        poller.StopAsync();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ testNetCore2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using MessagePack;
+using NetMQ;
+using NetMQ.Sockets;
+
+namespace testNetCore2
+{
+    class Program
+    {
+        private static bool _isConnected;
+        
+        static void Main(string[] args)
+        {
+            _isConnected = false;
+            
+            using (var poller = new NetMQPoller())
+            using (var pubSocket = new PublisherSocket())
+            {
+                var connectionString = "tcp://localhost:5555";
+                using (var repSocket = new ResponseSocket(connectionString))
+                {
+                    var tcpLocalhost = "tcp://localhost:12345";
+                    pubSocket.Bind(tcpLocalhost);
+
+                    pubSocket.SendReady += (sender, eventArgs) =>
+                    {
+                        if (!_isConnected)
+                        {
+                            NetMQMessage message = new NetMQMessage();
+                            message.Append(new NetMQFrame("CONNECT", Encoding.UTF8));
+                            message.Append(new NetMQFrame(connectionString, Encoding.UTF8));
+                            //Console.WriteLine("Sending connect Message...");
+                            eventArgs.Socket.SendMultipartMessage(message);
+                        }
+                        else
+                        {
+                            for (int i = 0; i < 100; i++)
+                            {
+                                NetMQMessage message = new NetMQMessage();
+                                if (i % 2 == 0)
+                                {
+                                    message.Append(new NetMQFrame("TopicA", Encoding.UTF8));
+                                }
+                                else
+                                {
+                                    message.Append(new NetMQFrame("TopicB", Encoding.UTF8));
+                                }
+
+                                var topicMessage = $"Message {i + 1}";
+                                var compressedMessage = MessagePackSerializer.Serialize(topicMessage);
+                                message.Append(new NetMQFrame(compressedMessage, compressedMessage.Length));
+                                Console.WriteLine($"Sending: {message[0].ConvertToString(Encoding.UTF8)} {topicMessage}({compressedMessage.Length})");
+                                eventArgs.Socket.SendMultipartMessage(message);
+                            }
+
+                            _isConnected = false;
+                        }
+                    };
+
+                    repSocket.ReceiveReady += (sender, eventArgs) =>
+                    {
+                        var message = eventArgs.Socket.ReceiveMultipartMessage();
+                    
+                        Console.WriteLine($"{message[0].ConvertToString()}:{message[1].ConvertToString()}");
+                    };
+
+                    repSocket.SendReady += (sender, eventArgs) =>
+                    {
+                        eventArgs.Socket.SendFrame("OK");
+                        _isConnected = true;
+                    };
+                
+                    poller.Add(pubSocket);
+                    poller.Add(repSocket);
+                
+                    poller.RunAsync();
+                    Console.WriteLine("Running Server...");
+                
+                    Console.ReadLine();
+                    poller.StopAsync();
+                }
+            }
+        }
+    }
+}
